39df1f642f9440f392d67cf2495c5864014ee644
Ali5hahriar <hemadiali@gmail.com>
2022-12-18 00:11:05 +0000

    Refrencing rules added

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 fn main() {
-    let s1 = String::from("Hello world");
-    let len = calculate_length(&s1); // We do not take ownership of s1, we just refrencing to it, in other word we 'barrow' it.
-    println!("The length of '{}' is {}", s1, len);
+    let refrence_to_nothing = dangle();
 }
 
-fn calculate_length(s: &String) -> usize {
-    let length = s.len(); // len() return the length of a String
-    length
+fn dangle() -> &String {
+    let s = String::from("Hello");
+
+    &s
 }
+
+// The Rules of Refrences
+// 1. At any given time, you can have either one mutable refrence 
+// or any numbe of immutable refrences.
+// 2. Refrences must always be valid.
